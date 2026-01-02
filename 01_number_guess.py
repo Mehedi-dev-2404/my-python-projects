21c1b523d05e4a6be6a125c04c36b58c78163890
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-02 23:20:59 +0000

    Add number guessing game script
    Introduces a Python script for a number guessing game where the player tries to guess a randomly generated number between 1 and 100, receiving feedback on each guess until correct.

---
+++ 01_number_guess.py
@@ -0,0 +1,23 @@
+# The player will try to guess a randomly generated number between 1 and 100.
+# The program will provide feedback on whether the guess is too low, too high, or correct.
+# The game continues until the player guesses the correct number.
+import random
+
+number = random.randint(1, 100)
+attempt = 1
+print("Welcome to number guessing game!!")
+guess = int(input("Guess a number between (1-100): "))
+
+while guess != number:
+    guess = input("Guess a number between (1-100)")
+    if guess != number:
+        if guess < number:
+            print("Incorrect guess, Low")
+        elif guess > number:
+            print("Incorrect guess, High")
+    
+    guess = input("Guess a number between (1-100)")
+    attempt += 1
+
+print(f"You win! The number was {number}")
+print(f"It took you {attempt} attempts.")
