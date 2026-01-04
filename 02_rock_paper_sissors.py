4e1bcc4664a2cd982ef8616863bf7132fbad00d6
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-04 11:31:51 +0000

    Refactor rock-paper-scissors game loop
    Added a game loop to allow repeated play until 'Q' is entered, included a welcome message, and implemented a game counter. Improved input handling and feedback for invalid choices.

--- 02_rock_paper_sissors.py
+++ 02_rock_paper_sissors.py
@@ -1,25 +1,33 @@
 import random
+print("WELCOME TO THE LEGENDARY ROCK SCISSORS AND PAPER GAME")
 
-player_input = input("Give your choice: (Rock, Paper, Scissors): ").capitalize
+player_input = 0
+game_counter = 0
 
-bot_list = ["ROCK", "SCISSORS", "PAPER"]
+while player_input != "Q":
 
-bot_input = random.choice(bot_list)
+    player_input = input("Give your choice: (Rock, Paper, Scissors): ").upper()
+    bot_list = ["ROCK", "SCISSORS", "PAPER"]
+    game_counter += 1
+    bot_input = random.choice(bot_list)
 
-if player_input == bot_input:
-    print("Draw")
-if player_input == "ROCK":
-    if bot_input == "PAPER":
-        print("You Lose!")
-    elif bot_input == "SCISSORS":
-        print("You Win")
-elif player_input == "PAPER":
-    if bot_input == "SCISSORS":
-        print("You Lose!")
-    elif bot_input == "ROCK":
-        print("You Win")
-elif player_input == "SCISSORS":
-    if bot_input == "ROCK":
-        print("You Lose!")
-    elif bot_input == "PAPER":
-        print("You Win")
+    if player_input != "ROCK" or "SCISSORS" or "PAPER":
+        if player_input == bot_input:
+            print("Draw")
+        if player_input == "ROCK":
+            if bot_input == "PAPER":
+                print("You Lose!")
+            elif bot_input == "SCISSORS":
+                print("You Win")
+        elif player_input == "PAPER":
+            if bot_input == "SCISSORS":
+                print("You Lose!")
+            elif bot_input == "ROCK":
+                print("You Win")
+        elif player_input == "SCISSORS":
+            if bot_input == "ROCK":
+                print("You Lose!")
+            elif bot_input == "PAPER":
+                print("You Win")
+        print(f"This is your {game_counter} game")
+    else: print("INVALID INPUT! Plese enter one of (Rock, Paper, Scissors)")         
