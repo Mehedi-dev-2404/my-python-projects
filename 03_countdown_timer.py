52121281b4abbb94510e7f88b1668e15487c47f6
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-06 17:27:46 +0000

    Update 03_countdown_timer.py

--- 03_countdown_timer.py
+++ 03_countdown_timer.py
@@ -4,12 +4,13 @@
 
 import time
 time_sec = 0
-try:
-    time_sec = int(input("Input time in seconds: "))
-    if time_sec < 0:
-        print("The number cannot be negative")
-except ValueError:     
-    print("Not an integer.")
+while time_sec > 0:
+    try:
+        time_sec = int(input("Input time in seconds: "))
+        if time_sec < 0:
+            print("The number cannot be negative")
+    except ValueError:     
+        print("Not an integer.")
 
 for i in range(time_sec, 0 , -1):
     print(f"⏳ {i} seconds left")
