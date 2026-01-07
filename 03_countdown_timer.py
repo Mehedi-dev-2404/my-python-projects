688947cc6924d19e7d54957640969c1ae1bd2a0b
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-07 21:52:20 +0000

    Add progress bar to countdown timer output
    Enhanced the countdown timer by displaying a visual progress bar alongside the time remaining. The timer now also prints a message when time is up.

--- 03_countdown_timer.py
+++ 03_countdown_timer.py
@@ -7,8 +7,10 @@ time_sec = 0
 
 def play():
     while True:
+        bar_length = 20
         try:
             time_sec = int(input("Input time in seconds: "))
+            total_time = time_sec
             if time_sec < 0:
                 print("The number cannot be negative")
                 continue
@@ -19,11 +21,12 @@ def play():
             print("Not an integer.")
             continue
 
-        for i in range(time_sec, 0 , -1):
+        for i in range(time_sec, -1 , -1):
+                filled_block = int((i / total_time) * bar_length)
                 minutes = i // 60
                 seconds = i % 60
-                print(f"⏳ {minutes:02}:{seconds:02}")
+                print(f"⏳ {minutes:02}:{seconds:02} | " + "█" * (bar_length - filled_block) + "░" * filled_block) 
                 time.sleep(1)
         break
-
+    print("⏰ Time’s up!")
 play()
