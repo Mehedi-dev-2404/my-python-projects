9e7677a489dc25b87bceb9309075834612293758
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-10 22:54:29 +0000

    Add file categorization by extension in organizer script
    Introduced a mapping of file extensions to categories and updated the script to categorize files in the target folder. The script now prints each file's category and stores lowercase filenames in a dictionary for further processing.

--- 07_organize_file.py
+++ 07_organize_file.py
@@ -1,10 +1,38 @@
 import os
 
-folder_path = 'test_folder' # Replace with your folder name
+folder_path = 'test_folder' 
 
-# Get a list of every file and folder inside
 files = os.listdir(folder_path)
+data = {}
+file_type = {
+    "images": [".jpg", ".png", ".jpeg", ".gif"],
+    "videos": [".mp4", ".mkv"],
+    "musics": [".mp3", ".wav"],
+    "zip": [".zip", ".tgz", ".rar", ".tar"],
+    "documents": [".pdf", ".docx", ".csv", ".xlsx", ".pptx", ".doc", ".ppt", ".xls"],
+    "setup": [".msi", ".exe"],
+    "programs": [".py", ".c", ".cpp", ".php"],
+    "design": [".xd", ".psd"]
+    }
+
 
 print(f"Files in '{folder_path}':")
+
 for file in files:
-    print(file)
+    full_path = os.path.join(folder_path, file)
+
+    if os.path.isfile(full_path):
+        lower_file = file.lower()
+        data[file] = lower_file
+
+for original_file, lower_file in data.items():
+    extension = os.path.splitext(lower_file)
+
+    if extension in file_type:
+        category = file_type[extension]
+    else:
+        category = "Others"
+
+    print(f"{original_file} → {category}")
+
+print(data)
