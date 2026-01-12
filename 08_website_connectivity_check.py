7c68871bb47d07f1e33995010de7fbf96205edc0
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-12 21:46:50 +0000

    Add loop and improved error handling to connectivity check
    Refactored the script to repeatedly prompt for website URLs until 'exit' is entered. Added automatic 'https://' prefix if missing and improved error handling for timeouts, connection errors, and invalid requests, providing more informative output.

--- 08_website_connectivity_check.py
+++ 08_website_connectivity_check.py
@@ -1,8 +1,28 @@
 import requests
+while True:
+    url = input("Enter website URL (or 'exit'): ")
+    if url == "exit":
+        print("Goodbye")
+        break
 
-url = input("Enter website URL: ")
-try:
-    response = requests.get(url)
-    print("Status code: ", response.status_code)
-except:
-    print("Website is not reachable")
+    if "https://" in url:
+        url = url
+    else:
+        url = "https://" + url
+
+    try:
+        response = requests.get(url, timeout=5)
+        print("Status code: ", response.status_code)
+        if response.status_code == 200:
+            print(f"UP {response.status_code}")
+        else:
+            print("Down (not reachable)")
+
+    except requests.ReadTimeout:
+        print("DOWN (Timeout)")
+
+    except requests.ConnectionError:
+        print("DOWN (Connection Error)")
+
+    except requests.RequestException:
+        print("DOWN (Invalid request)")
