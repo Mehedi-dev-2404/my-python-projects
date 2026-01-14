6a7da0b0468acf9a3ca1b16a00164e14498f130f
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-01-14 22:03:45 +0000

    Add file existence check to QR code generator
    Updated 09_qr_code_generator.py to prompt for data and output file name, and added a check to prevent overwriting existing files. Also added lol, lol.png, and water.png files.

--- 09_qr_code_generator.py
+++ 09_qr_code_generator.py
@@ -1,7 +1,16 @@
 import qrcode
+import os
 
-data = "https://google.com"
-img = qrcode.make(data)
-img.save("qr.png")
+data = input("Enter the data to encode: ")
+file_name = input("Enter the name of the file: ")
+
+output_file = f"{file_name}.png"
+
+if os.path.exists(file_name):
+    print(f"Stop '{file_name}' already exists. I won't overwrite it.")
+else:
+    img = qrcode.make(data)
+    img.save(f"{output_file}.png")
 
 print("QR code generated successfully")
+print(f"File address: /Users/mehedimostafa/Desktop/PROJECTS/New Projectxx/my-python-projects/{file_name}.png")
